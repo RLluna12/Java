0842feed56a2ca594ae5b0083a0fb5af601a6d70
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2023-04-26 23:19:24 -0300

    Add files via upload

---
+++ Interface/teste.java
@@ -0,0 +1,20 @@
+
+package uscs;
+
+
+public class teste {
+    
+    public static void main(String[] args) {
+        
+        Circulo a = new Circulo();
+        Quadrado b = new Quadrado();
+        
+        System.out.println(a.area(10, 20));
+        System.out.println(a.comprimento(10, 20));
+        
+        System.out.println(b.area(10, 20));
+        System.out.println(b.comprimento(10, 20));
+        
+    }
+    
+}
