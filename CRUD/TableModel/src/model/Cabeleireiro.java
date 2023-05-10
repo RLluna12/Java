49715900f989a8b044264187628b4b09bd790602
RLluna12 <110608226+RLluna12@users.noreply.github.com>
2023-05-09 23:37:46 -0300

    Add files via upload

---
+++ CRUD/TableModel/src/model/Cabeleireiro.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author rllun
+ */
+public class Cabeleireiro extends AbstractTableModel {
+
+    private List<Cliente> dados = new ArrayList<>();
+    private String[] colunas = {"Nome", "Corte", "Valor"};
+
+    @Override
+    public String getColumnName(int column) {
+        return colunas[column];
+    }
+
+    
+    @Override
+    public int getRowCount() {
+        return dados.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int linha, int coluna) {
+
+        switch (coluna) {
+            case 0:
+                return dados.get(linha).getNome();
+            case 1:
+                return dados.get(linha).getCorte();
+            case 2:
+                return dados.get(linha).getValor();
+
+        }
+        return null;
+    }
+
+    @Override
+    public void setValueAt(Object valor, int linha, int coluna) {
+        
+        switch (coluna) {
+            case 0:
+                 dados.get(linha).setNome((String) valor);
+                 break;
+            case 1:
+                 dados.get(linha).setCorte(((String) valor));
+                 break;
+            case 2:
+                 dados.get(linha).setValor(Double.parseDouble((String)valor));
+                 break;
+        }
+        this.fireTableRowsUpdated(linha, linha);
+    }
+    
+    
+    public void addRow(Cliente p){
+        this.dados.add(p);
+        this.fireTableDataChanged();
+    }
+
+    public void removeRow(int linha){
+        this.dados.remove(linha);
+        this.fireTableRowsDeleted(linha, linha);
+    }
+}
---
+++ CRUD/TableModel/src/model/Cliente.java
@@ -0,0 +1,39 @@
+
+package model;
+
+import javax.swing.JOptionPane;
+
+
+public class Cliente {
+    private String nome;
+    private String corte;
+    private double valor;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCorte() {
+        return corte;
+    }
+
+    public void setCorte(String corte) {
+        this.corte = corte;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+
+            this.valor = valor;
+        
+        
+        
+    }
+}
